893095967514cab9ac70a6f93b3d1986fd924fb5
Aleksandr Nikitin <alex@morbo.ru>
2020-07-04 01:59:40 +0300

    fix tests

--- Tests/ReactLoopTest.php
+++ Tests/ReactLoopTest.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace Morbo\React\Loop;
 
+use Morbo\React\Loop\Service\Loop;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\HttpKernel\Kernel;
 
 class ReactLoopTest extends KernelTestCase
 {
-    public function testDefault()
+    public function testDependencyInjection()
     {
         self::bootKernel();
-        $container = self::$kernel->getContainer();
+        $container = self::$container;
 
-        $this->assertTrue($container->has('react.loop'), 'React loop extension is loaded');
+        $this->assertTrue($container->has('react.loop'), '"react.loop" is loaded');
+        $this->assertTrue($container->has(Loop::class), '"Loop::class" is loaded');
     }
 }
 
