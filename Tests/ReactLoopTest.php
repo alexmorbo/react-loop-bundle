14490e8e5ce4db5ca8ffd59ad0603f8f029f0140
Aleksandr Nikitin <alex@morbo.ru>
2020-07-04 01:43:49 +0300

    fix tests

--- Tests/ReactLoopTest.php
+++ Tests/ReactLoopTest.php
@@ -2,20 +2,43 @@
 
 declare(strict_types=1);
 
-namespace Morbo\React\Redis;
+namespace Morbo\React\Loop;
 
-use Morbo\React\Loop\DependencyInjection\ReactLoopExtension;
-use PHPUnit\Framework\TestCase;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
-class ReactLoopTest extends TestCase
+class ReactLoopTest extends KernelTestCase
 {
     public function testDefault()
     {
-        $container = new ContainerBuilder();
-        $loader = new ReactLoopExtension();
-        $loader->load([[]], $container);
+        self::bootKernel();
+        $container = self::$kernel->getContainer();
 
-        $this->assertTrue($container->hasDefinition('react.loop'), 'React loop extension is loaded');
+        $this->assertTrue($container->has('react.loop'), 'React loop extension is loaded');
+    }
+}
+
+class TestingKernel extends Kernel
+{
+    public function __construct()
+    {
+        parent::__construct('test', true);
+    }
+
+    public function registerBundles()
+    {
+        return [
+            new ReactLoopBundle(),
+        ];
+    }
+
+    public function getCacheDir()
+    {
+        return __DIR__.'/cache/'.spl_object_hash($this);
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
     }
 }
